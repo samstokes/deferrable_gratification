0b86496f818071ad7a5df97d2e837c1c1a8601cc
Sam Stokes <sam@rapportive.com>
2011-01-14 18:48:08 -0800

    Test rewriting a series of nested binds as a chain works as expected...
    i.e. that Bind behaves as a monadic bind operator as intended.
    
    It only works this way when passing a block that returns a Deferrable:
    misusing #bind! like #map breaks this law and can cause confusion.
    (Specifically, if a block returns something other than a Deferrable,
    calling #bind! on that thing breaks us out of the monad - e.g. with a
    NoMethodError - so the nested syntax doesn't really make sense.)

--- spec/deferrable_gratification/combinators/bind_spec.rb
+++ spec/deferrable_gratification/combinators/bind_spec.rb
@@ -163,6 +163,92 @@ describe DeferrableGratification::Combinators::Bind do
   end
 
 
+  describe 'nested bind! equivalent to chained bind! (monad law: associativity)' do
+    # This sounds complicated, but basically means you can swap between a
+    # nested sequence of bind! calls and a chained sequence of bind! calls
+    # with the same blocks in the same order.  The latter may be more readable
+    # and also improves encapsulation (since the nested version leaks variables
+    # into inner scopes by creating closures).
+
+    class << self
+      def good_op(x); DG.const(x).tap(&:go); end
+      def bad_op(*_); DG.failure(RuntimeError, 'Oops').tap(&:go); end
+      def transform(x); x.succ; end
+      def boom(*_); raise 'Boom!'; end
+    end
+
+    # Apologies for the nasty metaprogramming but I can't find a better way of
+    # testing all the cases.
+    #
+    # For the cases => false, the two syntaxes should *not* behave the same:
+    # the nested syntax doesn't really make sense for these cases, because
+    # bind! no longer behaves as a monad and thus the values being passed to
+    # the blocks don't support the bind! operator.
+    {
+      %w(good_op good_op good_op)     => true,
+
+      %w(good_op good_op   transform) => true,
+
+      %w(good_op transform   good_op) => false,
+      %w(good_op transform transform) => false,
+
+      %w(good_op good_op  bad_op)     => true,
+      %w(good_op  bad_op good_op)     => true,
+      %w( bad_op good_op good_op)     => true,
+
+      %w(good_op good_op boom)        => true,
+      %w(good_op boom good_op)        => true,
+    }.map do |methods, should_be_equivalent|
+      [methods.map {|m| method m }, should_be_equivalent]
+    end.each do |(first, second, third), should_be_equivalent|
+      describe %-#{first.name}(0).bind! do |x|
+                   #{second.name}(x)
+                 end.bind! do |y|
+                   #{third.name}(y)
+                 end- do
+        subject do
+          first[0].bind! do |x|
+            second[x]
+          end.bind! do |y|
+            third[y]
+          end
+        end
+
+        it %-should #{should_be_equivalent ? '' : 'NOT '}behave the same as:
+                #{first.name}(0).bind! do |x|
+                  #{second.name}(x).bind! do |y|
+                    #{third.name}(y)
+                  end
+                end- do
+          success = SpecTools::ResultReceiver.new
+          failure = SpecTools::ResultReceiver.new
+
+          nested = first[0].bind! do |x|
+                     second[x].bind! do |y|
+                       third[y]
+                     end
+                   end
+
+          nested.callback {|result| success.result = result }
+          nested.errback {|error| failure.result = error }
+
+          behave_the_same = if success.has_result?
+            succeed_with(success.result)
+          else
+            fail_with(failure.result.class, failure.result.message)
+          end
+
+          if should_be_equivalent
+            subject.should behave_the_same
+          else
+            subject.should_not behave_the_same
+          end
+        end
+      end
+    end
+  end
+
+
   describe '.setup! without a block' do
     specify '.new should raise ArgumentError' do
       lambda { described_class.new(first_deferrable) }.
